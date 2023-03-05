d7563d6769f4fe0f74d0bc4d081ec6a62820097c
baustin91 <baustin14@my.gcu.edu>
2023-03-05 10:28:30 -0700

    Added validation to login page.

--- CLC_Milestone2/src/main/java/com/gcu/controllers/LoginController.java
+++ CLC_Milestone2/src/main/java/com/gcu/controllers/LoginController.java
@@ -1,7 +1,10 @@
 package com.gcu.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +19,19 @@ public class LoginController {
 	public String displayLogin(Model model) {
 		
 		model.addAttribute("loginModel", new LoginModel());
-		return "login.html";
+		return "Login.html";
 	}
 	
 	@PostMapping("/processLogin")
-	public String processLogin(LoginModel loginModel, Model model) {
+	public String processLogin(@Valid LoginModel loginModel, BindingResult bindingResult, Model model) {
+		
+		
+		if(bindingResult.hasErrors()) 
+		{
+			model.addAttribute("loginModel", loginModel);
+			return "Login.html";
+		}
+		
 		
 		model.addAttribute("loginModel", loginModel);
 		return "Dashboard.html";
