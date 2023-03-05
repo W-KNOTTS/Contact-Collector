957750c881b3c05896ea8aee58954f13d4c4dcd8
WillK <williamwknotts@gmail.com>
2023-03-05 16:47:15 -0700

    updates

--- CLC_Milestone2/src/main/java/com/gcu/controllers/LoginController.java
+++ CLC_Milestone2/src/main/java/com/gcu/controllers/LoginController.java
@@ -1,6 +1,7 @@
 package com.gcu.controllers;
 
-import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,7 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.gcu.models.ContactModel;
 import com.gcu.models.LoginModel;
+import com.gcu.models.RegModel;
+//import com.gcu.models.RegModel;
 
 @Controller
 @RequestMapping("/login")
@@ -19,22 +23,40 @@ public class LoginController {
 	public String displayLogin(Model model) {
 		
 		model.addAttribute("loginModel", new LoginModel());
-		return "Login.html";
+		return "login.html";
+	}
+	
+	@GetMapping("/2")
+	public String displayReg(Model model1) 
+	{
+		
+		model1.addAttribute("regModel", new RegModel());
+		return "UserReg.html";
 	}
 	
 	@PostMapping("/processLogin")
-	public String processLogin(@Valid LoginModel loginModel, BindingResult bindingResult, Model model) {
+	public String processLogin(LoginModel loginModel, BindingResult bindingResult, Model model) 
+	{
 		
+		List<ContactModel> user = new ArrayList<ContactModel>();
+		user.add(new ContactModel(0L, "William1", "Knotts", "315-489-9107", "wknotts1@my.gcu.edu", "1230 address"));
+		user.add(new ContactModel(1L, "William2", "Knotts", "315-489-9107", "wknotts2@my.gcu.edu", "1231 address"));
+		user.add(new ContactModel(2L, "William3", "Knotts", "315-489-9107", "wknotts3@my.gcu.edu", "1232 address"));
+		user.add(new ContactModel(3L, "William4", "Knotts", "315-489-9107", "wknotts4@my.gcu.edu", "1233 address"));
+		user.add(new ContactModel(4L, "William5", "Knotts", "315-489-9107", "wknotts5@my.gcu.edu", "1234 address"));
+
 		
-		if(bindingResult.hasErrors()) 
-		{
-			model.addAttribute("loginModel", loginModel);
-			return "Login.html";
-		}
+		model.addAttribute("title", "My Contacts");
+		model.addAttribute("user", user);
 		
-		
-		model.addAttribute("loginModel", loginModel);
-		return "Dashboard.html";
+		return "user";
 	}
+	@PostMapping("/processReg")
+	public String processReg(RegModel regModel, BindingResult bindingResult, Model model1) 
+	{
+		return "UserReg";
+		
+	}
+	
 	
 }
--- CLC_Milestone2/src/main/java/com/gcu/models/LoginModel.java
+++ CLC_Milestone2/src/main/java/com/gcu/models/LoginModel.java
@@ -1,18 +1,12 @@
 package com.gcu.models;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-
 public class LoginModel {
 
-	@NotNull(message="Username is a required field")
-	@Size(min=5, max=15, message="Username must be between 5 and 15 characters")
 	private String username;
-	
-	@NotNull(message="Password is a required field")
-	@Size(min=8, max=15, message="Password must be between 8 and 15 characters")
 	private String password;
-	public String getUsername() {
+	
+	public String getUsername() 
+	{
 		return username;
 	}
 	public void setUsername(String username) {
