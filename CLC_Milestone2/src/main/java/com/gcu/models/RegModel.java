5b4240992a3e532952470e4afcbf232f31a46a2c
WillK <williamwknotts@gmail.com>
2023-03-05 20:48:45 -0700

    updates

--- CLC_Milestone2/src/main/java/com/gcu/models/RegModel.java
+++ CLC_Milestone2/src/main/java/com/gcu/models/RegModel.java
@@ -109,24 +109,24 @@ public class RegModel
 	    this.address = address;
 	  }	 
 		
-		public String getUsername() 
-		{
+	  public String getUsername() 
+	  {
 			return username;
-		}
+	  }
 		
-		public void setUsername(String username) 
-		{
+	  public void setUsername(String username) 
+	  {
 			this.username = username;
-		}
+	  }
 		
-		public String getPassword() 
-		{
-			return password;
-		}
+	  public String getPassword() 
+	  {
+		  return password;
+	  }
 		
-		public void setPassword(String password) 
-		{
+	  public void setPassword(String password) 
+	  {
 			this.password = password;
-		}
+	  }
 	
 }
